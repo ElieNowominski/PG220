6be8934a0128983413943f8c768753dbbeeda210
enowominski <56869782+enowominski@users.noreply.github.com>
2020-11-16 16:28:36 +0100

    mini modif

--- Etape1/src/grille/Coin.java
+++ Etape1/src/grille/Coin.java
@@ -4,7 +4,7 @@ public class Coin extends CoinType{
     int x;
     int y;
 
-    Public Coin(int type, int x, int y){
+    public Coin(int type, int x, int y){
         super(type);
         this.x = x;
         this.y = y;
